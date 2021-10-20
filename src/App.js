3e320d57b9943613ec2069173e8e0fca0c7d9cde
noel99l <noel99l@noel99lMac-mini.local>
2021-10-20 22:11:53 +0900

    use State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0) //分割代入
+
+  // const increment = () => setCount(count + 1)
+  // const decrement = () => setCount(count - 1)
+
+  const increment = () => setCount(previousCount => previousCount + 1)
+  const decrement = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(count * 2)
+  const divide3 = () => {
+    setCount(previousCount=>{
+      if(previousCount % 3 === 0){
+        return previousCount / 3
+      } else {
+        return previousCount
+      }
+    })
+  }
   return (
-    <div>
-      This is a template for React App
-    </div>
+    <>
+      <div>
+        count: {count}
+      </div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={double}>x2</button>
+      <button onClick={divide3}>3の倍数の時だけ3で割る</button>
+    </>
   )
 }
 
